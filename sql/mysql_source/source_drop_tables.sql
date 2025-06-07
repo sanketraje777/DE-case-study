53b62b81f8dd3e836675e36ab13c216f9e1adb54
sanketraje777 <44047109+sanketraje777@users.noreply.github.com>
2025-06-07 10:47:26 +0530

    Delete sql directory

--- sql/mysql_source/source_drop_tables.sql
+++
@@ -1,6 +0,0 @@
-USE ounass_source;
-
--- Drop existing tables (in dependency order)
-DROP TABLE IF EXISTS salesorderitem;
-DROP TABLE IF EXISTS salesorder;
-DROP TABLE IF EXISTS customer;
--- sql/mysql_source/source_setup_database.sql
+++
@@ -1,3 +0,0 @@
--- Create source MySQL schema and tables
-CREATE DATABASE IF NOT EXISTS ounass_source;
-USE ounass_source;
